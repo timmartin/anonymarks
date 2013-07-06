ce53f840077429c3a41a4bf5d4b41e2c22a17ec2
Tim Martin <tim@asymptotic.co.uk>
2013-07-06 23:27:01 +0100

    Added a dummy view for showing bookmarks

--- anonymarks/views.py
+++ anonymarks/views.py
@@ -2,7 +2,13 @@ import os
 import boto
 
 from django.shortcuts import render
+from django.core.context_processors import csrf
 
 def home(request):
     context = {}
     return render(request, 'index.html', context)
+
+def show(request):
+    context = {}
+    context.update(csrf(request))
+    return render(request, 'show.html', context)
