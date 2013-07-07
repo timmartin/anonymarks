f68041a328142c6167832a7cc2d0edb5b2b8ec9d
Tim Martin <tim@asymptotic.co.uk>
2013-07-07 08:02:35 +0100

    The bookmarks list page now looks up bookmarks from the database (though currently there is no way to add new bookmarks, so it always shows up empty)

--- anonymarks/views.py
+++ anonymarks/views.py
@@ -1,8 +1,12 @@
 import os
-import boto
+import hashlib
 
 from django.shortcuts import render
 from django.core.context_processors import csrf
+
+from boto.dynamodb2.table import Table
+
+bookmarks_table = Table('bookmarks')
 
 def home(request):
     context = {}
@@ -10,5 +14,14 @@ def home(request):
 
 def show(request):
     context = {}
+
+    hash_obj = hashlib.sha1()
+    hash_obj.update(request.POST['passphrase'])
+
+    bookmarks = bookmarks_table.query(hash__eq=hash_obj.hexdigest())
+
+    context['bookmarks'] = {item.name : item.value
+                            for item in bookmarks}
+
     context.update(csrf(request))
     return render(request, 'show.html', context)
