8b4379e580c942713a9b010f0a34d78b7ea40ad9
Tim Martin <tim@asymptotic.co.uk>
2013-08-13 21:22:20 +0100

    Set the time zone to UTC

--- anonymarks/settings.py
+++ anonymarks/settings.py
@@ -15,11 +15,7 @@ MANAGERS = ADMINS
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
