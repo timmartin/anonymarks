078f1d0d9605412ff4048d3069582180c85cd397
Tim Martin <tim@asymptotic.co.uk>
2013-08-14 21:01:45 +0100

    Corrected the STATIC_URL so that the image is properly linked on the production server

--- anonymarks/settings.py
+++ anonymarks/settings.py
@@ -55,7 +55,7 @@ STATIC_ROOT = PROJECT_DIR + '/static_tmp'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://static.anonymarks.net/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
